7abcbbb2da1991084c01332b8fc27fe3611cc0cc
Guido Serra <guido.serra@olx.com>
2017-04-20 14:27:37 +0200

    Add StarCraft LIVE (remastered) +style/fix

--- Casks/starcraft.rb
+++ Casks/starcraft.rb
@@ -2,10 +2,12 @@ cask 'starcraft' do
   version 'LIVE'
   sha256 '144c0ce0483378764421e8cfb6098fcfe61c69e0898cbc4a225b7de8e4413b45'
 
+  # battle.net was verified as official when first introduced to the cask
   url "https://battle.net/download/getInstallerForGame?os=MAC&version=#{version}&gameProgram=STARCRAFT"
   name 'StarCraft'
   homepage 'https://starcraft.com/en-us/articles/20674424'
 
   container type: :zip
+
   app 'StarCraft Launcher.app'
 end
